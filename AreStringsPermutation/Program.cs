e6da25940adfa29ba58d60301883be871b1df698
unknown <sebastiengithub@gmail.com>
2025-05-17 17:10:30 +0200

    ReverseString

--- AreStringsPermutation/Program.cs
+++ AreStringsPermutation/Program.cs
@@ -4,26 +4,6 @@
     {
         static void Main(string[] args)
         {
-            string reverseStr = ReverseString2("Hello Brandon");
-        }
-
-        static string ReverseString(string str)
-        {
-            char[] strArray = str.Reverse().ToArray();
-            return new string(strArray);
-        }
-
-        static string ReverseString2(string str)
-        {
-            char[] strChars = str.ToCharArray();
-            char[] arrayResult = new char[strChars.Length];
-
-            for (int j = 0; j < strChars.Length; j++)
-            {
-                arrayResult[j] = strChars[strChars.Length - j - 1];
-            }
-
-            return new string(arrayResult);
         }
     }
 }
