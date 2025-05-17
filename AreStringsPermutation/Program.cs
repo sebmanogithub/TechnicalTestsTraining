39b3870a02baf106f19b4d6e58ba575ef043bdce
unknown <sebastiengithub@gmail.com>
2025-05-17 17:09:23 +0200

    ReverseString

--- AreStringsPermutation/Program.cs
+++ AreStringsPermutation/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string reverseStr = ReverseString2("Hello Brandon");
+        }
+
+        static string ReverseString(string str)
+        {
+            char[] strArray = str.Reverse().ToArray();
+            return new string(strArray);
+        }
+
+        static string ReverseString2(string str)
+        {
+            char[] strChars = str.ToCharArray();
+            char[] arrayResult = new char[strChars.Length];
+
+            for (int j = 0; j < strChars.Length; j++)
+            {
+                arrayResult[j] = strChars[strChars.Length - j - 1];
+            }
+
+            return new string(arrayResult);
         }
     }
 }
--- ReverseString/Program.cs
+++ ReverseString/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System.Text;
+
+static string ReverseString(string str)
+{
+    //var toto = str
+    //            .Reverse();
+    //string totostr = new string(toto.ToArray());
+
+    //char[] buffer = new char[str.Length];
+
+    //for (int i = 0; i < str.Length; i++)
+    //{
+    //    buffer[i] = str[str.Length - 1 - i];
+    //}
+
+    //return new string(buffer);
+
+    StringBuilder sb = new StringBuilder();
+    int i = str.Length - 1;
+
+    while (i >= 0)
+    {
+        sb.Append(str[i--]);
+    }
+
+    return sb.ToString();
+}
