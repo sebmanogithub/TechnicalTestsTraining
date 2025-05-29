072eb6f0c21ff3fdee74b8f4673d44f7bf91e00a
unknown <sebastiengithub@gmail.com>
2025-05-29 13:04:23 +0200

    PalidromeAddingNumber

---
+++ PalindromeAddingNumber/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace PalindromeAddingNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int step = FindStepsPalindrome(3523);
+        }
+
+        static int FindStepsPalindrome(long num)
+        {
+            if (IsPalindrome(num))
+            {
+                return 0;
+            }
+
+            int nbSteps = 0;
+            while (!IsPalindrome(num))
+            {
+                string str = num.ToString();
+                string reversedStr = new string(str.Reverse().ToArray());
+                num += long.Parse(reversedStr);
+                nbSteps++;
+            }
+
+            return nbSteps;
+        }
+
+        static bool IsPalindrome(long num)
+        {
+            string str = num.ToString();
+
+            for (int i = 0; i < str.Length / 2; i++)
+            {
+                if (str[i] != str[str.Length - 1 - i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static int palindromeChainLength(ulong n)
+        {
+            var reversed = ulong.Parse(string.Concat($"{n}".Reverse()));
+            return reversed == n ? 0 : 1 + palindromeChainLength(n + reversed);
+        }
+
+    }
+}
---
+++ Revisions/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Text;
+
+namespace Revisions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            bool IsOk = HasStringAllUniqueCharacters("ferdzste");
+            bool IsOk2 = HasStringAllUniqueCharacters("ferdzst");
+
+            string reverseString = ReverseString("ferdzst");
+            string newStr = SuppressAndOrder("jdemleo ekel kie");
+        }
+
+        static bool HasStringAllUniqueCharacters(string str)
+        {
+            HashSet<char> chars = new HashSet<char>();
+            foreach (char c in str)
+            {
+                if (!chars.Add(c))
+                {
+                    return false;
+                }
+            }
+            return true;
+
+            //return str.Distinct().Count() == str.Length;
+        }
+
+        static string SuppressAndOrder(string str)
+        {
+            var chars = str
+                        .ToLower()
+                        .Where(c => !char.IsWhiteSpace(c))
+                        .OrderBy(c => c)
+                        .ToArray();
+
+            return new string (chars);
+        }
+
+        static string ReverseString(string str)
+        {
+            char[] charStr = str.ToCharArray();
+            charStr.Reverse();
+
+            //return new string (charStr);
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < str.Length; i++)
+            {
+                sb.Append(str[str.Length - i - 1]);
+            }
+
+            //return sb.ToString();
+            int j = str.Length;
+
+            while (j > 0)
+            {
+                sb.Append(str[--j]);
+            }
+            s
+            return sb.ToString();
+        }
+    }
+}
